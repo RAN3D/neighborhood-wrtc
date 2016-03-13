8a80eeeb0bc1a994a7c441bd40cad82fd1bee187
chat-wane <grumpy.chat.wane@gmail.com>
2016-03-13 20:00:17 +0100

    callback function

--- example/main.js
+++ example/main.js
@@ -1,5 +1,6 @@
-var Neighborhood = require("neighborhood-wrtc");
+var Neighborhood = require('neighborhood-wrtc');
 
+// create 3 neighborhood tables without options
 var n1 = new Neighborhood({});
 var n2 = new Neighborhood({});
 var n3 = new Neighborhood({});
@@ -7,7 +8,9 @@ var n3 = new Neighborhood({});
 n1.connection(
     function(request){
         n2.connection(function(response){
-            n1.connection(null, response);            
+            n1.connection(function(){
+                console.log('n1->n2 connected');
+            }, response);            
         }, request);
     }
 );
@@ -15,7 +18,9 @@ n1.connection(
 n1.connection(
     function(request){
         n3.connection(function(response){
-            n1.connection(null, response);            
+            n1.connection(function(){
+                console.log('n1->n3 connected');
+            }, response);
         }, request);
     }
 );
